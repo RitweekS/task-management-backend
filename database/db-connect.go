129c0b53de20ca93e194cd8c24030e34cc9e3b91
ritweek <ritweek.kumar@bootlabstech.com>
2024-09-27 12:16:18 +0530

    fix:db connect

--- database/db-connect.go
+++ database/db-connect.go
@@ -2,15 +2,25 @@ package database
 
 import (
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 func InItDB(){
-	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5440 sslmode=disable TimeZone=Asia/Shanghai"
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	password := os.Getenv("DB_PASSWORD")
+
+	dsn := fmt.Sprintf("postgresql://neondb_owner:%s@ep-gentle-morning-a5ivmvpt.us-east-2.aws.neon.tech/neondb?sslmode=require",password)
+
 	db,err := gorm.Open(postgres.Open(dsn),&gorm.Config{})
 	if(err!=nil){
 		fmt.Println("Unable to connect database",err)
